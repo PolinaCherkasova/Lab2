611a5cf45a69c1414a2f4a7ea86dc872e860c2b8
Polina Cherkasova <cherkasovapolina971@yandex.ru>
2024-05-13 20:41:02 +0700

    Реализация паттерна Абстрактная фабрика: создание класса AbstractFactory и конкретных фабрик - CPlusPlusFactory, CSharpFactory, JavaFactory. В классе AbstractFactory объявлены методы createClass, createMethod, createPrintOperator, которые переопределены в CPlusPlusFactory, CSharpFactory, JavaFactory. Добавление комментариев.

--- AbstractFactory/AbstractFactory.pro
+++ AbstractFactory/AbstractFactory.pro
@@ -19,7 +19,11 @@ else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
 
 HEADERS += \
+    abstractfactory.h \
     classunit.h \
+    cplusplusfactory.h \
+    csharpfactory.h \
+    javafactory.h \
     methodunit.h \
     printoperatorunit.h \
     unit.h
---
+++ AbstractFactory/cplusplusfactory.h
@@ -0,0 +1,22 @@
+#ifndef CPLUSPLUSFACTORY_H
+#define CPLUSPLUSFACTORY_H
+#include "abstractfactory.h"
+
+class CPlusPlusFactory : public AbstractFactory //Конкретная фабрика CPlusPlusFactory, наследуемая от абстрактной, для создания продуктов на с++
+{
+public:
+    shared_ptr<ClassUnit> createClass(const string& name) { // переопределение метода для создания конкретного класса на с++ с заданным именем
+     //   return shared_ptr<ClassUnit> (new CPlusPlusClass(name));
+    }
+    shared_ptr<MethodUnit> createMethod(const string& name, const string& returnType, Unit::Flags flags) { // переопределение метода для создания
+    // конкретного метода на с++ с заданным именем, возвращаемым типом и флагом(static, const или virtual)
+     //   return shared_ptr<MethodUnit> (new CPlusPlusMethod(name, returnType, flags));
+    }
+    shared_ptr<PrintOperatorUnit> createPrintOperator(const string& text) { // переопределение метода для создания конкретного оператора вывода на с++
+    // с заданным текстом
+     //   return shared_ptr<PrintOperatorUnit> (new CPlusPlusPrintOperator(text));
+    }
+
+};
+
+#endif // CPLUSPLUSFACTORY_H
